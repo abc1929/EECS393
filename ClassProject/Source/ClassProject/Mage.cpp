5fe29e21ef20f6f9186e2116bffcbee210498714
Jiawei Wu <abc1929@gmail.com>
2016-09-19 12:01:46 -0400

    Update Mage.cpp

--- ClassProject/Source/ClassProject/Mage.cpp
+++ ClassProject/Source/ClassProject/Mage.cpp
@@ -12,6 +12,31 @@ AMage::AMage()
 
 }
 
+void AMage::SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent){
+	// Set up gameplay key bindings
+	check(PlayerInputComponent);
+	PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &ACharacter::Jump);
+	PlayerInputComponent->BindAction("Jump", IE_Released, this, &ACharacter::StopJumping);
+
+	PlayerInputComponent->BindAxis("MoveForward", this, &AMage::MoveForward);
+	PlayerInputComponent->BindAxis("MoveRight", this, &AMage::MoveRight);
+
+	// We have 2 versions of the rotation bindings to handle different kinds of devices differently
+	// "turn" handles devices that provide an absolute delta, such as a mouse.
+	// "turnrate" is for devices that we choose to treat as a rate of change, such as an analog joystick
+	PlayerInputComponent->BindAxis("Turn", this, &APawn::AddControllerYawInput);
+	PlayerInputComponent->BindAxis("TurnRate", this, &AMage::TurnAtRate);
+	PlayerInputComponent->BindAxis("LookUp", this, &APawn::AddControllerPitchInput);
+	PlayerInputComponent->BindAxis("LookUpRate", this, &AMage::LookUpAtRate);
+
+	// handle touch devices
+	PlayerInputComponent->BindTouch(IE_Pressed, this, &AMage::TouchStarted);
+	PlayerInputComponent->BindTouch(IE_Released, this, &AMage::TouchStopped);
+
+	// VR headset functionality
+	PlayerInputComponent->BindAction("ResetVR", IE_Pressed, this, &AMage::OnResetVR);
+}
+
 // Called when the game starts or when spawned
 void AMage::BeginPlay()
 {
@@ -27,9 +52,72 @@ void AMage::Tick( float DeltaTime )
 }
 
 // Called to bind functionality to input
-void AMage::SetupPlayerInputComponent(class UInputComponent* InputComponent)
-{
-	Super::SetupPlayerInputComponent(InputComponent);
+// void AMage::SetupPlayerInputComponent(class UInputComponent* InputComponent)
+// {
+// 	Super::SetupPlayerInputComponent(InputComponent);
 
+// }
+
+void AMage::OnResetVR()
+{
+	UHeadMountedDisplayFunctionLibrary::ResetOrientationAndPosition();
+}
+
+void AMage::TouchStarted(ETouchIndex::Type FingerIndex, FVector Location)
+{
+	// jump, but only on the first touch
+	if (FingerIndex == ETouchIndex::Touch1)
+	{
+		Jump();
+	}
+}
+
+void AMage::TouchStopped(ETouchIndex::Type FingerIndex, FVector Location)
+{
+	if (FingerIndex == ETouchIndex::Touch1)
+	{
+		StopJumping();
+	}
+}
+
+void AMage::TurnAtRate(float Rate)
+{
+	// calculate delta for this frame from the rate information
+	AddControllerYawInput(Rate * BaseTurnRate * GetWorld()->GetDeltaSeconds());
+}
+
+void AMage::LookUpAtRate(float Rate)
+{
+	// calculate delta for this frame from the rate information
+	AddControllerPitchInput(Rate * BaseLookUpRate * GetWorld()->GetDeltaSeconds());
+}
+
+void AMage::MoveForward(float Value)
+{
+	if ((Controller != NULL) && (Value != 0.0f))
+	{
+		// find out which way is forward
+		const FRotator Rotation = Controller->GetControlRotation();
+		const FRotator YawRotation(0, Rotation.Yaw, 0);
+
+		// get forward vector
+		const FVector Direction = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+		AddMovementInput(Direction, Value);
+	}
+}
+
+void AMage::MoveRight(float Value)
+{
+	if ( (Controller != NULL) && (Value != 0.0f) )
+	{
+		// find out which way is right
+		const FRotator Rotation = Controller->GetControlRotation();
+		const FRotator YawRotation(0, Rotation.Yaw, 0);
+	
+		// get right vector 
+		const FVector Direction = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+		// add movement in that direction
+		AddMovementInput(Direction, Value);
+	}
 }
 
