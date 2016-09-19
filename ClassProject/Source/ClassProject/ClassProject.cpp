fe5d69abbaa4151d6b7f02fcd7534e6d0876e676
Jiawei Wu <abc1929@Gmail.com>
2016-09-19 13:25:01 +0900

    test sync

---
+++ ClassProject/Source/ClassProject/ClassProject.cpp
@@ -0,0 +1,7 @@
+// Copyright 1998-2016 Epic Games, Inc. All Rights Reserved.
+
+#include "ClassProject.h"
+
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ClassProject, "ClassProject" );
+ 
