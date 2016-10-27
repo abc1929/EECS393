463ab7f3350f8e43c436684559ee6edfb5ed5810
Jiawei Wu <abc1929@Gmail.com>
2016-10-27 03:28:48 -0400

    rip

--- ClassProject/Intermediate/WindowsNoEditor/NativizedAssets/Intermediate/Build/Win64/UE4/Development/NativizedAssets/NativizedAssets.h
+++
@@ -1,5 +0,0 @@
-#ifndef __AUTO_NATIVIZEDASSETS_H__
-#define __AUTO_NATIVIZEDASSETS_H__
-//Last Write: 10/26/2016 04:38:13 +00:00
-#include "C:\Users\Jiawei\Documents\GitHub\EECS393\ClassProject\Intermediate\WindowsNoEditor\NativizedAssets\Source\NativizedAssets\Public\NativizedAssets.h"
-#endif//__AUTO_NATIVIZEDASSETS_H__
